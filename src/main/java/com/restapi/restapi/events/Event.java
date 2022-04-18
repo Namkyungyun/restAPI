bce30aa72d594a1e3b10a53315d38af927e78ab0
Yun Namkyung <nagy123@direa.co.kr>
2022-04-18 10:48:17 +0900

    controller

--- src/main/java/com/restapi/restapi/events/Event.java
+++ src/main/java/com/restapi/restapi/events/Event.java
@@ -2,12 +2,15 @@ package com.restapi.restapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 //Entity에다가는 @Data을 쓰면 안된다 -> 상호참조에 의해서 스택오버플로우가 발생할 여지가 있음.
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -21,5 +24,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 }
---
+++ src/test/java/com/restapi/restapi/events/EventControllerTests.java
@@ -0,0 +1,65 @@
+package com.restapi.restapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//단위 테스트라고 보기에는 dispatchServlet, eventCotroller, dataHandler, Converter들이 조합된 채로 동작되는 테스트이기에 단위기테스트라고 보기 어렵다.
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    //mock으로 만들어진 dispatchServlet (가짜 요청)
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
+    @Test
+    void createEvent() throws Exception{
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 04, 18, 10, 25))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022,04, 19, 22,10))
+                .beginEventDateTime(LocalDateTime.of(2022, 04, 18, 10, 25))
+                .endEventDateTime(LocalDateTime.of(2022,04, 19, 22,10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
+
+
+}
