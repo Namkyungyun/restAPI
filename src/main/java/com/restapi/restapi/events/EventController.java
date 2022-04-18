7b72827bbba22ebd5bbeae3bd6e700b7136d7377
Yun Namkyung <nagy123@direa.co.kr>
2022-04-18 12:47:52 +0900

    controller test

--- src/main/java/com/restapi/restapi/events/EventController.java
+++ src/main/java/com/restapi/restapi/events/EventController.java
@@ -2,19 +2,23 @@ package com.restapi.restapi.events;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
-@Controller
+@RestController
 @RequestMapping(value = "api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
@@ -26,7 +30,6 @@ public class EventController {
 
     @PostMapping
     public ResponseEntity createEvent(@RequestBody Event event) {
-
         Event newEvent = this.eventRepository.save(event);
 
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri(); //uri로 변환
--- src/test/java/com/restapi/restapi/events/EventControllerTests.java
+++ src/test/java/com/restapi/restapi/events/EventControllerTests.java
@@ -8,16 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //단위 테스트라고 보기에는 dispatchServlet, eventCotroller, dataHandler, Converter들이 조합된 채로 동작되는 테스트이기에 단위기테스트라고 보기 어렵다.
 @ExtendWith(SpringExtension.class)
@@ -53,12 +58,14 @@ public class EventControllerTests {
         Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .accept(MediaTypes.HAL_JSON)
+                        .contentType("application/hal+json;charset=UTF-8")
+                        .accept("application/hal+json;charset=UTF-8")
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists());
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json;charset=UTF-8"));
     }
 
 
